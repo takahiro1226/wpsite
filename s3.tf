f509f739fb68da3def507fc9240958853f25bb07
takahiro1226 <arzu.ruguz.beorc.night@gmail.com>
2026-01-19 05:56:38 +0900

    vpc　add

--- s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-wpsite-state-wpsite"
-
-  tags = {
-    Name        = "Terraform State Storage"
-    Environment = "Management"
-  }
-}
